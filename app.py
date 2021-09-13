f596ac199da0a75cb365de6468f69fe637336373
speedracer05 <speedracer-05@outlook.com>
2021-09-12 23:04:57 -0700

    app.py add run scrape func and update database

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from scrape_mars
 # Create an instance of Flask
 app = Flask(__name__)
 
-# Use PyMongo to establish Mongo connection
+# Use Flask Pymongo to establish Mongo connection
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 # Route to render index.html template using data from Mongo
@@ -16,3 +16,13 @@ def home():
     mars_dict = mongo.db.mars_dict.find_one()
     # Return template and data
     return render_template("index.html", mars=mars_dict)
+
+# Route that will trigger the scrape function
+@app.route("/scrape")
+def scrape():
+
+    # Run scrape function
+    mars_dict = mongo.db.scrape_info()
+
+    # Update Mongo database using update and upsert=True
+    mars_dict.db.collection.
