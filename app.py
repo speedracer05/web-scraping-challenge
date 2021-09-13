e308c76dbeae7b630ebf81fee5adf2997b397284
speedracer05 <speedracer-05@outlook.com>
2021-09-13 04:24:08 -0700

    index html. add news section

--- app.py
+++ app.py
@@ -2,35 +2,23 @@ from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 from scrape_mars
 
-# Create an instance of Flask
 app = Flask(__name__)
 
-# Use Flask Pymongo to establish Mongo connection
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
-# Route to render index.html template using data from Mongo
 @app.route("/")
-def home():
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
 
-    # Find one record of data from the mongo database
-    mars_dict = mongo.db.mars_dict.find_one()
-    # Return template and data
-    return render_template("index.html", mars=mars_dict)
 
-# Route that will trigger the scrape function
 @app.route("/scrape")
 def scrape():
+    mars = mongo.db.mars
+    mars_data = scrape_mars.scrape_all()
+    mars.update({}, mars_data, upsert=True)
+    return redirect('/', code=302)
 
-    mars_dict = mongo.db.mars_dict
-    
-    # Run scrape function
-   mars_data = scrape_mars.scrape()
-
-    # Update Mongo database using update and upsert=True
-    mars_dict.db.collection.update({}, mars_data, upsert=True)
-
-    # Redirect back to home page
-    return redirect("/")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
---
+++ scrape_mars.py
@@ -0,0 +1,111 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from splinter import Browser
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+import requests
+import pymongo
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import time
+
+executable_path = {'executable_path': ChromeDriverManager().install()}
+browser = Browser('chrome', **executable_path, headless=False)
+
+scraped_data = {}
+
+nasa_url = "https://redplanetscience.com" 
+browser.visit(nasa_url)
+time.sleep(5)
+
+html = browser.html
+soup = bs(html, 'html.parser')
+
+news_title = soup.find('div', class_='content_title').text
+news_p = soup.find('div', class_='article_teaser_body').text
+print(news_title)
+print(news_p)
+
+scrape_nasa_news={"Title":news_title, "Paragraph":news_p}
+scrape_nasa_news
+
+scraped_data ["Title"] = news_title
+scraped_data["Paragraph"] = news_p
+
+mars_url = "https://spaceimages-mars.com" 
+browser.visit( mars_url)
+image_html = browser.html
+
+soup = bs( image_html, "html.parser")
+
+featured_image = soup.find_all("img", class_ = "headerimage fade-in")[0]["src"]
+featured_image_url = mars_url + "/" + featured_image
+print(featured_image_url)
+
+jpl = {"img_url":featured_image_url}
+jpl
+
+scraped_data["img_url"] = featured_image_url
+
+browser.quit()
+
+facts_url = "https://galaxyfacts-mars.com/"
+facts_data = pd.read_html(facts_url)
+facts_data
+
+facts_df = facts_data[0]
+facts_table = facts_df.to_html(index=False)
+facts_table
+
+facts_table.replace("\n", "")
+print(facts_table)
+
+mars_facts = {"htmlTable":facts_data}
+
+scraped_data["htmlTable"] = facts_df.to_html
+
+
+executable_path = {'executable_path': ChromeDriverManager().install()}
+browser = Browser('chrome', **executable_path, headless=False)
+
+hem_url = 'https://marshemispheres.com/'
+browser.visit(hem_url)
+
+html = browser.html
+soup = bs(html, 'html.parser')
+
+items = soup.find_all('div', class_='item')
+
+hemi_urls = []
+hemi_title = []
+
+for item in items:
+    hemi_urls.append( hem_url + item.find('a')['href'])
+    hemi_title.append( item.find('h3').text.strip())
+
+print( hemi_urls)
+hemi_title
+
+hemi_img_urls = []
+
+for url in hemi_urls:
+    browser.visit(url)
+    html = browser.html
+    soup = bs(html, 'html.parser')
+    
+    source_url = hem_url + soup.find('img',class_='wide-image')['src']
+    hemi_img_urls.append( source_url)
+    
+hemi_img_urls
+
+usgs = []
+for i in range( len( hemi_title)):
+    usgs.append({ 'title':hemi_title[i], 'img_url':hemi_img_urls[i]})
+
+usgs
+
+browser.quit()
+
+mars_dict ={"news_title": news_title, "news_p": news_p, "featured_image_url": featured_image_url, 
+            "facts_table": facts_table, "hem_url":hem_url}
+mars_dict
