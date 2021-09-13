aacdac3713a92eba652cd212f7f8b1cd60d6ef1d
speedracer05 <speedracer-05@outlook.com>
2021-09-12 22:56:51 -0700

    app.py. add route, fine record of data from MongoDB, code that triggers scrape function

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-from scrape_mars import scrape
+from scrape_mars
 
 # Create an instance of Flask
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def home():
+
+    # Find one record of data from the mongo database
+    mars_dict = mongo.db.mars_dict.find_one()
+    # Return template and data
+    return render_template("index.html", mars=mars_dict)
